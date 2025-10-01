3c932141f3b05f3d831df82633ad6284cf1ebaeb
Astro <aymenaaymen123@gmail.com>
2025-10-01 23:49:14 +0100

    feat: Added the homepage with the surah listing SurahList component refactor: Update SurahCard component to use Link for navigation fix: Adjust sidebar styles for better layout and responsiveness chore: Create ReciteQuranLayout and RecitePage components for recitation feature style: Modify global styles to improve scrollbar visibility

--- src/components/surahs/surah-card.tsx
+++ src/components/surahs/surah-card.tsx
@@ -1,18 +1,22 @@
+"use client"
+
 import { Card, CardContent } from "@/components/ui/card";
 import { Surah } from "@/entities/surah";
+import Link from "next/link";
 
 export function SurahCard({ data, isSelected = false }: { data: Surah, isSelected?: boolean }) {
 
-    const bgColorSelected = isSelected ? 'bg-primary text-primary-foreground' : 'bg-muted';
+    const bgColorSelected = isSelected ? 'bg-success/50 text-success-foreground' : 'bg-muted';
 
     return (
-        <CardContent className={`bg-card flex p-2 w-full h-full justify-between gap-3 rounded-md hover:shadow-md hover:cursor-pointer ${isSelected ? 'border-2 border-muted-foreground/40' : ''}`}>
-            <div className={`h-full w-[3.5rem] ${bgColorSelected} rounded-md flex items-center justify-center`}>
+        <Link href={`/recite?surahId=${data.id}`} className={`bg-card shadow-sm hover:shadow-xl hover:scale-101 transition-all flex p-2 w-full h-max-[5rem] justify-between gap-3 rounded-md hover:shadow-md hover:cursor-pointer ${isSelected ? 'border-2 border-muted-foreground/40' : ''}`}>
+        <CardContent className={`bg-card shadow-sm hover:shadow-xl hover:scale-101 transition-all flex p-2 w-full h-max-[5rem] justify-between gap-3 rounded-md hover:shadow-md hover:cursor-pointer ${isSelected ? 'border-2 border-muted-foreground/40' : ''}`}>
+            <div className={`h-full w-[2.5rem] ${bgColorSelected} rounded-md flex items-center justify-center`}>
                 {data.id}
             </div>
             <div className="flex flex-col justify-center flex-1">
                 <p className="text-md font-semibold">{data.name_simple}</p>
-                <p className="text-sm text-muted-foreground">{data.translated_name.name}</p>
+                <p className="text-sm text-muted-foreground line-clamp-1">{data.translated_name.name}</p>
             </div>
             <div className={`h-full w-[2.5rem] ${bgColorSelected} rounded-md flex flex-col items-center justify-center`}>
                 <span className="text-[.725rem]">Ayat</span>
@@ -20,6 +24,7 @@ export function SurahCard({ data, isSelected = false }: { data: Surah, isSelecte
                 <span className="text-[.625rem]">{data.revelation_place}</span>
             </div>
         </CardContent>
+        </Link>
     )
 }
 
--- src/components/surahs/surah-list.tsx
+++ src/components/surahs/surah-list.tsx
@@ -1,13 +1,35 @@
-import { getChapters } from "@/actions/surahs.actions";
-import { SurahCard } from "@/components/surahs/surah-card";
+"use client"
 
-export async function SurahList() {
-    const { chapters: data } = await getChapters();
+import { SurahCard } from "@/components/surahs/surah-card";
+import { Input } from "@/components/ui/input";
+import { Surah } from "@/entities/surah";
+import React, { useMemo } from "react";
+import { useDebounce } from "use-debounce";
+
+export function SurahList({surahs}: {surahs: Surah[]}) {
+    const [searchQuery, setSearchQuery] = React.useState('');
+
+    // debounce the search input
+    const [debouncedSearchQuery] = useDebounce(searchQuery, 300);
+
+    const filteredSurahs = useMemo(() => {
+        if (!debouncedSearchQuery) return surahs;
+        return surahs.filter(surah =>
+            surah.name_simple.toLowerCase().includes(debouncedSearchQuery.toLowerCase()) ||
+            surah.translated_name.name.toLowerCase().includes(debouncedSearchQuery.toLowerCase()) ||
+            surah.id.toString() === debouncedSearchQuery
+        );
+
+    }, [surahs, debouncedSearchQuery]);
 
     return (
         <div className="w-full h-full">
-            <div className="grid grid-cols-1 md:grid-cols-3 w-full h-full gap-5">
-                {data.map((surah) => (
+            {/* Filtering and searching inputs */}
+            <div className="flex justify-center items-center my-5">
+                <Input value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} className="w-full md:max-w-[50%] text-center rounded-full border-muted border-2" placeholder="Search by page number or surah name"/>
+            </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 w-full h-full gap-5">
+                {filteredSurahs.map((surah) => (
                     <SurahCard key={surah.id} data={surah} isSelected={surah.id === 1}/>
                 ))}
             </div>
